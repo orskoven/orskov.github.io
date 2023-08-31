9583b1ee804cd17e953ab61991e076c719c8e5ef
orskoven <simo26k2@stud.kea.dk>
2023-08-31 20:22:44 +0200

    bpublished date

--- src/Post.js
+++ src/Post.js
@@ -1,14 +1,14 @@
 const Post1 = () => {
     return (
       <section className="section">
-            <h2>Finding a 3-month intership </h2>
-            <h5>Author: <a href="https://master.d2p1vjkxtgfh0z.amplifyapp.com">Simon Ø. D. Beckmann</a></h5>
-            <p>As a part of my education as an AP Computer Science student at KEA, i have to get an intership in my own business or in another business that operates in the field of informations systems.</p>
-            <p>I primarly focus on cyber security, because i am curious about the industry and have an opportunity to get the "top-up" in cyber security at KEA, by extending my education with 1,5 years. </p>
-            <p> Currently my eyes has fallen on the danish cyber security company <a href="https://www.dubex.dk/?utm_source=google&utm_medium=paid&utm_campaign=brand&utm_id=655700301570&utm_term=%7Bkeyword%7D&gclid=EAIaIQobChMIk7_N8fmGgQMVZlSRBR3r-wkREAAYASAAEgJb7PD_BwE" class="youtube-link">DUBEX</a>. I have had an opportunity to see Keld, who is working for the company, speak about threats and risk assesment at KEA and at a Data conference in Copenhagen last year.</p>
-            <p> Since I cannot get an intership with CFCS (Center For Cyber Sikkerhed), since they demand a background check, which they don't offer for interships, i am currently looking towards the private sector.</p>
-            <p> Besides DUBEX, <a href="https://certaintelligence.com/da/forside/?gclid=EAIaIQobChMIq9bv6PmGgQMVGAuiAx3ylgbmEAAYASAAEgKQiPD_BwE" class="youtube-link">CERTA</a> and IFCR (institut for cyber risk) are also interesting to work for. I am mostly familiar with IFCR. I first heard about them, when I read an article about the "famous" penetration tester Oliver Lyak. He was nominated for the Top 100 business persons under 30 by the danish news paper Berlingske in 2023. He currently works for IFCR and therefore i heard about them, and he has formly worked, and possibly also educated by FE (Forsvarets Efterretnings tjeneste) or CFCS from looking a he's linkedIn profile. To me IFCR, from looking at their website, sounds interesting in as a workplace with a holistic approach to work life balance. From what they say, they offer a 4-day work week.</p>
-      </section>
+     <h2>Finding a 3-Month Internship</h2>
+<h5>Published on August 31, 2023 by <a href="https://master.d2p1vjkxtgfh0z.amplifyapp.com">Simon Ø. D. Beckmann </a></h5>
+<p>As a part of my education as an AP Computer Science student at KEA, I have to get an internship in my own business or in another business that operates in the field of information systems.</p>
+<p>I primarily focus on cybersecurity because I am curious about the industry and have an opportunity to get the "top-up" in cybersecurity at KEA by extending my education with 1.5 years.</p>
+<p>Currently, my eyes have fallen on the Danish cybersecurity company <a href="https://www.dubex.dk/?utm_source=google&utm_medium=paid&utm_campaign=brand&utm_id=655700301570&utm_term=%7Bkeyword%7D&gclid=EAIaIQobChMIk7_N8fmGgQMVZlSRBR3r-wkREAAYASAAEgJb7PD_BwE" class="youtube-link">DUBEX</a>. I had an opportunity to see Keld, who is working for the company, speak about threats and risk assessment at KEA and at a Data conference in Copenhagen last year.</p>
+<p>Since I cannot get an internship with <a href="https://www.cfcs.dk" class="youtube-link">CFCS (Center For Cyber Sikkerhed)</a> due to their demand for a background check, which they don't offer for internships, I am currently looking towards the private sector.</p>
+<p>Besides DUBEX, <a href="https://certaintelligence.com/da/forside/?gclid=EAIaIQobChMIq9bv6PmGgQMVGAuiAx3ylgbmEAAYASAAEgKQiPD_BwE" class="youtube-link">CERTA</a> and <a href="https://ifcr.dk" class="youtube-link">IFCR (Institut for Cyber Risk)</a> are also interesting options to work for. I am mostly familiar with IFCR. 
+I first heard about them when I read an article about the "famous" penetration tester Oliver Lyak. He was nominated as one of the Top 100 business persons under 30 by the Danish newspaper Berlingske in 2023. He currently works for IFCR, and therefore, I heard about them. He has formerly worked and possibly been educated by FE (Forsvarets Efterretningstjeneste) or CFCS, as indicated by his LinkedIn profile. To me, IFCR, based on their website, sounds like an interesting workplace with a holistic approach to work-life balance. According to what they say, they offer a 4-day work week.</p> </section>
     );
   };
   
