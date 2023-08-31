2b388700dc7c1c63dcdffd6657d797839e99815d
orskoven <simo26k2@stud.kea.dk>
2023-08-31 19:57:26 +0200

    author

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,18 @@
-const AboutMe = () => {
+const Post1 = () => {
+    return (
+      <section className="section">
+            <h2>Finding a 3-month intership </h2>
+            <h5>Author: <a href="orskovbeckmann.com">Simon Ø. D. Beckmann</a></h5>
+            <p>As a part of my education as an AP Computer Science student at KEA, i have to get an intership in my own business or in another business that operates in the field of informations systems.</p>
+            <p>I primarly focus on cyber security, because i am cuorois about the industry and have an opportinuity to get the "top-up" in cyber security at KEA, by extending my education with 1,5 years. </p>
+            <p> Currently my eyes has fallen on the danish cyber security company <a href="https://www.dubex.dk/?utm_source=google&utm_medium=paid&utm_campaign=brand&utm_id=655700301570&utm_term=%7Bkeyword%7D&gclid=EAIaIQobChMIk7_N8fmGgQMVZlSRBR3r-wkREAAYASAAEgJb7PD_BwE" class="youtube-link">DUBEX</a>. I have had an opportunity to see Keld, who is working for the company, speak about threats and risk assesment at KEA and at a Data conference in Copenhagen last year.</p>
+            <p> Since I cannot get an intership with CFCS (Center For Cyber Sikkerhed), since they demand a background check, which they don't offer for interships, i am currently looking towards the private sector.</p>
+            <p> Besides DUBEX, <a href="https://certaintelligence.com/da/forside/?gclid=EAIaIQobChMIq9bv6PmGgQMVGAuiAx3ylgbmEAAYASAAEgKQiPD_BwE" class="youtube-link">CERTA</a> and IFCR (institut for cyber risk) are also interesting to work for. I am mostly familiar with IFCR. I first heard about them, when I read an article about the "famous" penetration tester Oliver Lyak. He was nominated for the Top 100 business persons under 30 by the danish news paper Berlingske in 2023. He currently works for IFCR and therefore i heard about them, and he has formly worked, and possibly also educated by FE (Forsvarets Efterretnings tjeneste) or CFCS from looking a he's linkedIn profile. To me IFCR, from looking at their website, sounds interesting in as a workplace with a holistic approach to work life balance. From what they say, they offer a 4-day work week.</p>
+      </section>
+    );
+  };
+  
+  const Post2 = () => {
     return (
       <section className="section">
             <h2>Finding a 3-month intership </h2>
@@ -11,5 +25,7 @@ const AboutMe = () => {
     );
   };
   
-  export default AboutMe;
+
+
+  export default Post1;
   
