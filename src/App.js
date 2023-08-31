ecbefc512d19c099918f618035b6b6e0dc793972
orskoven <simo26k2@stud.kea.dk>
2023-08-31 15:01:13 +0200

    first post

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './App.css';
-import AboutMe from './AboutMe';
-import Skills from './Skills';
-import Projects from './Projects';
+import Post from './Post';
+
 
 const App = () => {
   return (
     <div className="container">
       <header className="header">
-        <h1>Welcome to Simon's Portfolio</h1>
+        <h1>Spilled Beans</h1>
         <p className="intro-text">
         </p>
       </header>
       <main className="content">
-  <AboutMe />
-  <Skills />
-  <Projects />
+  <Post />
 </main>
     </div>
   );
