846308aaf342a6d511780e0adacc9b90188c62a6
orskoven <simo26k2@stud.kea.dk>
2023-09-01 15:58:16 +0200

    published add and wiki link to nlp

--- src/Post2.js
+++ src/Post2.js
@@ -4,13 +4,15 @@
     return (
       <section className="section">
             <h2>To certify or degree</h2>
+            <h5>Published on September 1, 2023 by <a href="https://master.d2p1vjkxtgfh0z.amplifyapp.com">Simon Ø. D. Beckmann </a></h5>
+
             <p>An eyebrow-lifting newsletter bumped into my mail app this morning. I often don't read many of the newsletters I get, but this one addressed an interesting and current point I've been traversing the internet and several podcasts to find guidance about.</p>
 
 <p>To sum up my wonderings into a question, which is most attractive to a future employee, a professional certificate or a degree? The newsletter was brought to me by Coursera, which stated that often employers narrow down the candidates for certain jobs by making a bachelor's degree mandatory, which I can confirm in most of where I have been looking for software positions in mostly big companies, but I must admit it isn't everywhere when it comes to software engineering jobs.</p>
 
 <p>Another important point the newsletter brought, or a good way to equip the reader and potential candidate for a job that could require either a degree or where a certificate could replace that requirement, is to look at current available job listings for certificate or degree requirements when the candidate is about to find a job. This could help the candidate decide if a certificate could match the degree requirement or if it is certain a degree is required.</p>
 
-<p>I have actually managed to find a podcast, <a href="https://ocdevel.com/mlg" class="youtube-link">Machine Learning Guide</a>. The speaker has actually managed to go around the degree requirement for working with NLP. He had a background as a web designer and managed to use Coursera, Udemy, and possibly more to upskill and get a job that required a master's degree. He also revealed that he also worked hard by communicating his findings into that podcast, which is brilliant as an audio guide in Machine Learning, helped him get the job.</p>
+<p>I have actually managed to find a podcast, <a href="https://ocdevel.com/mlg" class="youtube-link">Machine Learning Guide</a>. The speaker has actually managed to go around the degree requirement for working with <a href="https://en.wikipedia.org/wiki/Natural_language_processing" class="youtube-link">NLP</a>. He had a background as a web designer and managed to use Coursera, Udemy, and possibly more to upskill and get a job that required a master's degree. He also revealed that he also worked hard by communicating his findings into that podcast, which is brilliant as an audio guide in Machine Learning, helped him get the job.</p>
 
 <p>Since I am looking for an internship within cybersecurity addressed beforehand, I am really curious about what has the higher value to an employee within this field. My best guess, from reading <a href="https://www.reddit.com/r/dkfinance/comments/wbpvpx/hvad_får_man_i_løn_med_en_datamatiker_top-up/" class="youtube-link">this Reddit post</a>, books, <a href="https://www.linkedin.com/learning/cybersecurity-careers-and-certifications/how-to-get-cybersecurity-experience?u=36836804" class="youtube-link">LinkedIn Learning courses</a> that many in the cybersecurity value certificates.</p>
 
