bde3ebfb28c6e999c187674912bce5e2c864aa56
orskoven <simo26k2@stud.kea.dk>
2023-09-01 15:52:04 +0200

    added post 2

--- src/Post.js
+++ src/Post.js
@@ -15,13 +15,19 @@ I first heard about them when I read an article about the "famous" penetration t
   const Post2 = () => {
     return (
       <section className="section">
-            <h2>Finding a 3-month intership </h2>
-            <p>As a part of my education as an AP Computer Science student at KEA, i have to get an intership in my own business or in another business that operates in the field of informations systems.</p>
-            <p>I primarly focus on cyber security, because i am cuorois about the industry and have an opportinuity to get the "top-up" in cyber security at KEA, by extending my education with 1,5 years. </p>
-            <p> Currently my eyes has fallen on the danish cyber security company <a href="https://www.dubex.dk/?utm_source=google&utm_medium=paid&utm_campaign=brand&utm_id=655700301570&utm_term=%7Bkeyword%7D&gclid=EAIaIQobChMIk7_N8fmGgQMVZlSRBR3r-wkREAAYASAAEgJb7PD_BwE" class="youtube-link">DUBEX</a>. I have had an opportunity to see Keld, who is working for the company, speak about threats and risk assesment at KEA and at a Data conference in Copenhagen last year.</p>
-            <p> Since I cannot get an intership with CFCS (Center For Cyber Sikkerhed), since they demand a background check, which they don't offer for interships, i am currently looking towards the private sector.</p>
-            <p> Besides DUBEX, <a href="https://certaintelligence.com/da/forside/?gclid=EAIaIQobChMIq9bv6PmGgQMVGAuiAx3ylgbmEAAYASAAEgKQiPD_BwE" class="youtube-link">CERTA</a> and IFCR (institut for cyber risk) are also interesting to work for. I am mostly familiar with IFCR. I first heard about them, when I read an article about the "famous" penetration tester Oliver Lyak. He was nominated for the Top 100 business persons under 30 by the danish news paper Berlingske in 2023. He currently works for IFCR and therefore i heard about them, and he has formly worked, and possibly also educated by FE (Forsvarets Efterretnings tjeneste) or CFCS from looking a he's linkedIn profile. To me IFCR, from looking at their website, sounds interesting in as a workplace with a holistic approach to work life balance. From what they say, they offer a 4-day work week.</p>
-      </section>
+            <h2>To certify or degree</h2>
+            <p>An eyebrow-lifting newsletter bumped into my mail app this morning. I often don't read many of the newsletters I get, but this one addressed an interesting and current point I've been traversing the internet and several podcasts to find guidance about.</p>
+
+<p>To sum up my wonderings into a question, which is most attractive to a future employee, a professional certificate or a degree? The newsletter was brought to me by Coursera, which stated that often employers narrow down the candidates for certain jobs by making a bachelor's degree mandatory, which I can confirm in most of where I have been looking for software positions in mostly big companies, but I must admit it isn't everywhere when it comes to software engineering jobs.</p>
+
+<p>Another important point the newsletter brought, or a good way to equip the reader and potential candidate for a job that could require either a degree or where a certificate could replace that requirement, is to look at current available job listings for certificate or degree requirements when the candidate is about to find a job. This could help the candidate decide if a certificate could match the degree requirement or if it is certain a degree is required.</p>
+
+<p>I have actually managed to find a podcast, <a href="https://ocdevel.com/mlg" class="youtube-link">Machine Learning Guide</a>. The speaker has actually managed to go around the degree requirement for working with NLP. He had a background as a web designer and managed to use Coursera, Udemy, and possibly more to upskill and get a job that required a master's degree. He also revealed that he also worked hard by communicating his findings into that podcast, which is brilliant as an audio guide in Machine Learning, helped him get the job.</p>
+
+<p>Since I am looking for an internship within cybersecurity addressed beforehand, I am really curious about what has the higher value to an employee within this field. My best guess, from reading <a href="https://www.reddit.com/r/dkfinance/comments/wbpvpx/hvad_får_man_i_løn_med_en_datamatiker_top-up/" class="youtube-link">this Reddit post</a>, books, <a href="https://www.linkedin.com/learning/cybersecurity-careers-and-certifications/how-to-get-cybersecurity-experience?u=36836804" class="youtube-link">LinkedIn Learning courses</a> that many in the cybersecurity value certificates.</p>
+
+<p>I have also read an article from Computer World <a href="https://www.computerworld.dk/tag/innovation" class="youtube-link">here</a> that, other than relevant proficiency in some tools like programming at Google, is not valued as much as being able to connect with people. To conclude, I assume that in terms of pursuing a cybersecurity career, a "top-up" is not necessary.</p>
+ </section>
     );
   };
   
