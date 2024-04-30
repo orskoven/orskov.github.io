a3029a9724ad63bb493ef878e13bc64fd61a1020
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2024-04-30 10:31:30 +0200

    Update Toolbar.js
    update toolbar links

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -6,13 +6,15 @@ const Toolbar = () => {
     <div className="toolbar">
       <div className="toolbar-logo">
         {/* Replace with your logo or site name */}
-        <span>Your Logo</span>
+        <span>Binary Beans</span>
       </div>
       <div className="toolbar-buttons">
         {/* Add your toolbar buttons here */}
-        <button className="toolbar-button">Cyber Security</button>
-        <button className="toolbar-button">IT & Economics</button>
+        <a href="https://orange-rock-004fad41e-preview.westus2.5.azurestaticapps.net" className="toolbar-button">Talk with Cyber Security Assistant</a>
+        <a href="https://orange-smoke-0c522e91e-preview.westus2.5.azurestaticapps.net" className="toolbar-button">Discover NIST AI.RMF</a>
+        <a href="https://master.d2p1vjkxtgfh0z.amplifyapp.com" className="toolbar-button">Portfolio</a>
         {/* Add more buttons as needed */}
+        <a href="https://www.youtube.com/@binarybeans" className="toolbar-button">Binary Beans YouTube</a>
       </div>
     </div>
   );
