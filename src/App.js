745220009d9fc82c37724d276635599dc4bbab6c
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2024-05-01 11:49:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Post1 from './Post1';
 import Post2 from './Post2';
 import Post3 from './Post3';
 import Post4 from './Post4';
+import Post5 from './Post5';
 import Toolbar from './Toolbar';
 
 
@@ -16,6 +17,7 @@ const App = () => {
         </p>
       </header>
       <main className="content">
+            <Post5 />
     <Post4 />
   <Post3 />
   <Post2 />
