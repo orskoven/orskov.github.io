9207ef8142695f600375382b7ed7fc2c572df1ff
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2024-06-18 14:07:52 +0200

    Create Post13.js

---
+++ src/Post13.js
@@ -0,0 +1,8 @@
+const Post13 = () => {
+  return (
+    <section className="section">
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1PqAxcrzB6XMDMrgSXY3b5?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>    </section>
+  );
+}
+
+export default Post10;
